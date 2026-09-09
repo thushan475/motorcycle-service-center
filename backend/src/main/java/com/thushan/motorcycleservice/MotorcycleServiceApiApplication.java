c1177f45c93e42d5ae6cffbe5584d9fffbab3550
thushan475 <thushanpahasara04@gmail.com>
2026-09-09 15:50:36 +0530

    feat: initialize spring boot application

---
+++ backend/src/main/java/com/thushan/motorcycleservice/MotorcycleServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.thushan.motorcycleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorcycleServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorcycleServiceApiApplication.class, args);
+    }
+
+}
